a19c9a0c5e79a10212d1ecb092838b4989043283
aashvibusa <aashvibusa@gmail.com>
2024-02-24 14:21:31 -0600

    fixed SimpleTex api call

--- SimpleTex/SimpleTex_scripts/test.py
+++ SimpleTex/SimpleTex_scripts/test.py
@@ -1,18 +1,20 @@
 import requests
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-@app.route("/")
-def get_request():
+@app.route("/get_SimpleTex/<gloop>")
+def get_request(gloop):
     #copying from simpletex
-    api_url="https://server.simpletex.cn/xxxxx" # interface address
-    data = {...} # request data
+    api_url="https://server.simpletex.cn/api/latex_ocr/" # interface address
+    data = {} # request data
     header={"token":"yxz25lSItTKzj596L2tIVSjziF6k5HVy9Zs3LSNHeiaK4WtXWJ6D301hzPc7XPBJ"} # Authentication information, use UAT method here
-    file=[("file",("test_image.jpeg",open("test_image.jpeg", 'rb')))] # request file, field name is usually file
+    # gloop = request.args.get("file") 
+    file=[("file",("./SimpleTex_scripts/" + gloop ,open("./SimpleTex_scripts/" + gloop, 'rb')))] # request file, field name is usually file
     res = requests.post(api_url, files=file, data=data, headers=header) # Use the requests library to upload files
     print(res.status_code)
     print(res.text)
+    return jsonify(res.text)
     
     
