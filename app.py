77d802a675450ba473863cc7344598664b70105a
Kai Karadi <kaikaradi23@gmail.com>
2024-02-24 14:08:41 -0800

    emrge

--- app.py
+++ app.py
@@ -1,8 +1,25 @@
 from flask import Flask, request, jsonify
 from openai import OpenAI
+import requests
 import json
 
 app = Flask(__name__)
+
+
+@app.route("/get_SimpleTex/<gloop>")
+def get_request(gloop):
+    #copying from simpletex
+    api_url="https://server.simpletex.cn/api/latex_ocr/" # interface address
+    data = {} # request data
+    header={"token":"yxz25lSItTKzj596L2tIVSjziF6k5HVy9Zs3LSNHeiaK4WtXWJ6D301hzPc7XPBJ"} # Authentication information, use UAT method here
+    # gloop = request.args.get("file") 
+    file=[("file",("./SimpleTex_scripts/" + gloop ,open("./SimpleTex_scripts/" + gloop, 'rb')))] # request file, field name is usually file
+    res = requests.post(api_url, files=file, data=data, headers=header) # Use the requests library to upload files
+    print(res.status_code)
+    print(res.text)
+    return jsonify(res.text)
+    
+    
 
 # Define your processing function
 def process_text(question):
--- frontend/chiba-math/src/app/page.tsx
+++ frontend/chiba-math/src/app/page.tsx
@@ -1,9 +1,49 @@
-import Image from "next/image";
+"use client";
+import "./Home.css";
+import { useState } from "react";
 
 export default function Home() {
+  const [file, setFile] = useState<File>();
+
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    console.log('calling req');
+    e.preventDefault();
+    if (!file) return;
+
+    try {
+      const data = new FormData();
+      data.set("file", file);
+      console.log('sending');
+      const res = await fetch("/api/upload", {
+        method: "POST",
+        body: data,
+      });
+      // handle the error
+      if (!res.ok) throw new Error(await res.text());
+    } catch (e: any) {
+      // Handle errors here
+      console.error(e);
+    }
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div><p>HI</p></div>
+    <main>
+      <div className="banner">Chiba Math</div>
+      <div className="grid">
+        <div className="block1">
+          Flex 1
+          <form onSubmit={onSubmit}>
+            <input
+              type="file"
+              name="file"
+              onChange={(e) => setFile(e.target.files?.[0])}
+            />
+            <input type="submit" value="Upload" />
+          </form>
+        </div>
+        <div className="block2">Flex 2</div>
+      </div>
     </main>
   );
 }
+// }
